50e9c929d28c715f76ce97b5363994940211d684
Abdulquadri Adegbiji <adegbijilanre@gmail.com>
2023-03-12 12:29:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import RandomText from "./components/RandomText";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <RandomText />
+      <h2>Start editing to see some magic happen!</h2>
+      <RandomText />
+    </div>
+  );
+}
---
+++ src/components/RandomText.js
@@ -0,0 +1,9 @@
+const RandomText = () => {
+  return (
+    <h1>
+      “You can't go back and change the beginning but you can start where you
+      are and change the ending.” ― C.S. Lewis{" "}
+    </h1>
+  );
+};
+export default RandomText;
